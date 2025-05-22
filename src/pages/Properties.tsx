da497bb70790c7b007900fa792dd0046db9cb1c0
Leo-ia310 <ventatormenta@gmail.com>
2025-05-21 23:48:52 -0200

    Fotos nuevas

--- src/pages/Properties.tsx
+++ src/pages/Properties.tsx
@@ -11,7 +11,7 @@ const allProperties: PropertyType[] = [
   {
     id: 1,
     title: "Finca de 30 Manzanas",
-    imageUrl: "../public/ImagenesFinca/Finca30.webp",
+    imageUrl: "../public/ImagenesFinca/Finca30.jpeg",
     price: 250000,
     location: "San rafael del sur",
     size: 30,
